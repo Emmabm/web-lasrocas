ac1373bb81d340e49273cba7a6b4490e213167dd
Emmanuel <emmanuelbustos292@gmail.com>
2025-09-06 15:44:02 -0300

    subidavercel

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  'https://gcfjmwnlbpnltguwfrwz.supabase.co',       // 👈 Reemplazalo con tu URL real del proyecto
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZmptd25sYnBubHRndXdmcnd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NDk2ODAsImV4cCI6MjA2MjMyNTY4MH0.qZWy5RdFJtMYl0L7a_neKl7xsaTYg1ubnAhbZw2wpfw' // 👈 Pegá ahí esa clave "anon" completa
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
